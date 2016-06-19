1f622af33b6e81f1d6ddb930abb659df04a2aa90
Thomas Hernandez <thomasah12@ymail.com>
2016-06-19 10:57:56 -0500

    some cross platform support and better handling for difrent file types

--- music_player.py
+++ music_player.py
@@ -12,9 +12,9 @@ def make_list(list_path):
                 song_list.append(full_path)
 
 
-def player(player_path):
-    mplayer = r'C:\Users\Tom\Documents\Python_Projects\music_player\MPlayer\mplayer.exe "'
-    process = mplayer + player_path + '"'
+def player(song_path):
+    mplayer = r'C:\Users\Tom\Documents\Python_Projects\music_player\MPlayer\mplayer.exe'
+    process = '{} "{}"'.format(mplayer, song_path)
     subprocess.Popen(process, stdout=subprocess.PIPE).stdout.read()
 
 
@@ -24,8 +24,9 @@ if __name__ == '__main__':
     while True:
         random.shuffle(song_list)
         for path in song_list:
-            artist = path.split('\\')[3]
-            album = path.split('\\')[4]
-            song_name = path.split('\\')[5]
-            print('Artist: ' + artist + ' - Album: ' + album + ' - Song: ' + song_name[:-5])
+            path_parts = path.split(os.sep)
+            artist = path_parts[3]
+            album = path_parts[4]
+            song_name = os.path.splitext(path_parts[5])[0]
+            print('Artist: {} - Album: {} - Song: {}'.format(artist, album, song_name))
             player(path)
